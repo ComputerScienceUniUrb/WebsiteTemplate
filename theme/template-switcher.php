f1786bd778aa469df7ce7be2636f980da46e4987
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2023-07-03 21:57:46 +0200

    Add new site switcher

--- theme/template-switcher.php
+++ theme/template-switcher.php
@@ -1,109 +1,109 @@
 <?php /* Template Name: Version picker */
 
-get_header('redux');
+get_header('clean');
 ?>
 
 <style type="text/css">
-#inner-content {
-    margin-top: 0;
-}
+    body {
+        background: #ffffff;
+    }
 
-.picker h1 {
-    text-align: center;
-}
-.row {
-    display: flex;
-    flex-direction: column;
-}
-.badge {
-    width: 100%;
-}
-.badge .image {
-    width: calc(100% - 40px);
-    margin: 0 20px;
-}
-.badge .image > img {
-    width: 100%;
-    height: auto;
-    border-radius: 100%;
-    box-shadow: 0 2px 12px rgba(100,100,100,0.2);
-    transform: translateY(-0px);
-    transition: box-shadow 2s ease-out, transform 1s ease-out;
-}
-.badge:hover .image > img {
-    box-shadow: 0 2px 36px rgba(100,100,100,0.7);
-    transform: translateY(-8px);
-}
-.badge .subtitle {
-    text-align: center;
-    font-size: 2.7em;
+    #content,
+    #inner-content {
+        margin-top: 0 !important;
+        margin-bottom: 0 !important;
+    }
 
-    text-shadow: 1px 1px 0 rgba(80,80,80,0.0);
-    transition: text-shadow 1.4s ease-out;
-}
-.badge:hover .subtitle {
-    text-shadow: 1px 1px 6px rgba(80,80,80,0.3);
-}
-.badge a {
-    color: gray;
-    text-decoration: none;
-}
-.badge.triennale a .subtitle {
-    color: #fe7d3c;
-}
-.badge.magistrale a .subtitle {
-    color: #2db970;
-}
+    #content {
+        min-height: 100vh;
+        display: flex;
+        flex-direction: column;
+    }
 
-@media only screen and (min-width: 768px) {
+    #footer-cover {
+        background: #dadada;
+        flex-grow: 1;
+
+        font-size: .75rem;
+        line-height: 1.6;
+
+        padding: 3rem 2rem 1rem 2rem;
+        text-align: center;
+        color: #333;
+    }
+    #footer-cover > div {
+        margin-bottom: .8rem;
+    }
+    #footer-cover a {
+        color: #444;
+        text-decoration: none;
+        transition: color .2s;
+    }
+    #footer-cover a:hover {
+        color: #666;
+        text-decoration: underline;
+    }
+
     .row {
-        flex-direction: row;
+        display: flex;
+        flex-direction: column;
     }
-    .badge {
-        width: 50%;
+
+    .six {
+        width: 100%;
     }
-}
+    .six .image {
+        font-size: 0;
+    }
+    .six .image img {
+        width: 100%;
+        height: auto;
+    }
+
+    @media only screen and (min-width: 768px) {
+        .row {
+            flex-direction: row;
+        }
+        .six {
+            width: 50%;
+        }
+        #footer-cover {
+            font-size: .9rem;
+        }
+    }
 </style>
 
 <div id="content">
 
-    <div id="inner-content" class="wrap clearfix picker">
-
-        <div id="main" class="twelve first clearfix" role="main">
-
-            <h1>
-                <b>Informatica Applicata</b><br />
-                Il tuo percorso
-            </h1>
-
-        </div>
+    <div>
 
         <div class="row">
-            <div class="six badge triennale">
+            <div class="six triennale">
                 <a href="https://informatica.uniurb.it/triennale/">
-                    <div class="image">
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/square-informatica-applicata-magistrale.jpg" alt="Corso Triennale in Informatica Applicata" />
-                    </div>
-                    <div class="subtitle">
-                        Triennale
-                    </div>
+                    <div class="image"><img src="<?php echo get_template_directory_uri(); ?>/images/cdl-switcher-triennale-informatica-scienza-e-tecnologia.jpg" alt="Corso Triennale in Informatica Applicata" /></div>
                 </a>
             </div>
 
-            <div class="six badge magistrale">
-            <a href="https://informatica.uniurb.it/magistrale/">
-                    <div class="image">
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/square-informatica-applicata-triennale.jpg" alt="Corso Magistrale in Informatica Applicata" />
-                    </div>
-                    <div class="subtitle">
-                        Magistrale
-                    </div>
+            <div class="six magistrale">
+                <a href="https://informatica.uniurb.it/magistrale/">
+                    <div class="image"><img src="<?php echo get_template_directory_uri(); ?>/images/cdl-switcher-magistrale-informatica-e-innovazione-digitale.jpg" alt="Corso Magistrale in Informatica Applicata" /></div>
                 </a>
             </div>
         </div>
 
     </div>
 
+    <div id="footer-cover">
+        <div>Corsi di laurea presso l’<a href="https://www.uniurb.it/">Università degli Studi di Urbino</a>.</div>
+        <div>
+            Scegli il tuo percorso:<br />
+            <a href="https://informatica.uniurb.it/triennale/">Laurea triennale in Informatica Scienza e Tecnologia</a><br />
+            <a href="https://informatica.uniurb.it/magistrale/">Laurea magistrale in Informatica e Innovazione Digitale</a>
+        </div>
+    </div>
+
 </div>
+
+<footer class="footer" role="contentinfo"></footer>
 
 <?php get_footer('redux');
